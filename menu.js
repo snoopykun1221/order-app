45ac913dd33ab193cc8e7eb917341ffde46eb653
snoopykun1221 <nna17220@gmail.com>
2026-02-15 19:49:22 +0900

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-// menu.js
-export const MENU = [
-  { id: "curry", name: "カレー", price: 900 },
-  { id: "ramen", name: "ラーメン", price: 950 },
-  { id: "gyoza", name: "餃子", price: 450 },
-  { id: "beer", name: "ビール", price: 600 },
-  { id: "cola", name: "コーラ", price: 250 },
-];
